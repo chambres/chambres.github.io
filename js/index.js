a4a9495c8f4a69a33ba195dae14d8cabb81d26f0
chambres <rahulsaravana04@gmail.com>
2024-01-08 23:54:41 -0600

    initial commit

---
+++ js/index.js
@@ -0,0 +1,437 @@
+
+
+picture = `https://garden.spoonflower.com/c/12586917/p/f/m/mzsRzXPkH9fUD88ZyZvbXe5H2ptnXjKce4zUUDNhXBaNIBEAZwgG/Dark%20Grey%20Solid%20Color.jpg`
+cards =
+{
+    'Advanced YT Music Downloader': {
+        url: 'https://github.com/chambres/YT-Music-Downloader',
+        description: 'Download and edit metadata of your favorite songs from Youtube',
+        tags: ['python', 'pyqt5', 'yt-dlp', 'ffmpeg'],
+        year: 2024
+    },
+    'OBS Bluetooth Heartrate': {
+        url: 'https://github.com/chambres/OBSbluetoothheartrate',
+        description: 'Use a bluetooth heartrate monitor to show BPM in OBS',
+        tags: ['python', 'obs', 'bluetooth', 'bitstruct'],
+        year: 2022
+    },
+    'Schedule Reaper':{
+        url: 'https://github.com/chambres/ScheduleReaping',
+        description: "An oversight in KatyISD's schedule manager allowed me to get the schedule of any student",
+        tags: ['python', 'requests', 'flask'],
+        year: 2022
+    },
+    'Smartbook Automation':{
+        url: 'https://github.com/chambres/smartbookAutomation',
+        description: "Automatically complete your Smartbook homework",
+        tags: ['python', 'selenium', 'automation'],
+        year: 2022
+    },
+    'Sapling Automation':{
+        url: 'https://github.com/chambres/saplingAutomation',
+        description: "Automatically complete your Sapling homework",
+        tags: ['python', 'selenium', 'automation'],
+        year: 2022
+    },
+    'Midi Soundboard':{
+        url: 'https://github.com/chambres/smartbookAutomation',
+        description: "A soundboard that can be controlled via MIDI piano",
+        tags: ['python', 'midi', 'pygame'],
+        year: 2022
+    },
+
+    'Sliding Puzzle': {
+        url:'https://github.com/chambres/sliding-puzzle',
+        description: 'A sliding puzzle game in Unity',
+        tags: ['unity', 'c#', 'puzzle game'],
+        year: 2022
+    },
+    'Othello':{
+        url:'https://github.com/chambres/othellounity',
+        description: 'A sliding puzzle game in Unity',
+        tags: ['unity', 'c#', 'othello', 'go'],
+        year: 2022
+    },
+    'Run':{
+        url:'https://github.com/chambres/RunUnity',
+        description: 'Remake of the game Run in Unity',
+        tags: ['unity', 'c#', 'run', '3D'],
+        year: 2023
+    },
+    '2D Infinite Runner':{
+        url:'https://github.com/chambres/Infinite-Runner-Unity',
+        description: 'A 2D infinite runner game in Unity',
+        tags: ['unity', 'c#', 'infinite runner', '2D'],
+        year: 2022
+    },
+    'hacRedux':{
+        url:'https://github.com/chambres/hacredux',
+        description: "A ground-up remake of KatyISD's Home Access Center",
+        tags: ['javascript', 'html', 'css', 'python'],  
+        year: 2021
+    },
+    'Multiplayer Dots and Boxes': {
+        url:'https://github.com/chambres/NetDotsAndBoxes',
+        description: 'A multiplayer dots and boxes game in Java using sockets',
+        tags: ['java', 'jframe', 'networking', 'multiplayer'],
+        year: 2023
+    },
+    'sharpshooter': {
+        url:'https://github.com/chambres/sharpshooter',
+        description: 'Couch Co-op game of reaction time and speed. Wrote low-level code to achieve dual keyboard support.',
+        tags: ['unity', 'c#', '3D', 'couch co-op'],
+        year: 2023
+    },
+    'Simple Chatroom': {
+        url:'https://github.com/chambres/javachatroom',
+        description: 'Simple chatroom in Java using sockets',
+        tags: ['java', 'jframe', 'networking'],
+        year: 2023
+    },
+    'NetConnect4': {
+        url: 'https://github.com/chambres/NetConnect4',
+        description: 'Connect 4 in Java using sockets',
+        tags: ['java', 'jframe', 'networking'],
+        year: 2023
+    },
+    'School Manager':{
+        url: 'https://github.com/chambres/SchoolManager',
+        description: 'A school management system in Java',
+        tags: ['java', 'jframe'],
+        year: 2023
+    },
+    'TryItHACExtension':{
+        url: 'https://github.com/chambres/tryitHACExtension',
+        description: 'Test out grades in Home Access Center',
+        tags: ['javascript', 'chrome extension'],
+        year: 2023
+    },
+    'Minesweeper':{
+        url: 'https://github.com/chambres/JavaMinesweeper',
+        description: 'A minesweeper game in Java',
+        tags: ['java', 'jframe'],
+        year: 2023
+    },
+    'Simple Sidescroller':{
+        url: 'https://github.com/chambres/sidescrollerunity',
+        description: 'A sidescroller game in Unity',
+        tags: ['unity', 'c#', '2D'],
+        year: 2023
+    },
+    'Gravity Game':{
+        url: 'https://github.com/chambres/Gravity-Game',
+        description: 'A gravity game in Unity',
+        tags: ['unity', 'c#', '2D'],
+        year: 2022
+    },
+    'Homebridge Remote Pauser':{
+        url: 'https://github.com/chambres/Homebridge-Remote-Pauser',
+        description: 'Pause any media with Homebridge',
+        tags: ['javascript', 'python', 'homebridge'],
+        year: 2022
+    },
+    'Comment To Function Parser':{
+        url: 'https://github.com/chambres/commentParser',
+        description: 'Convert Java comments to functions',
+        tags: ['javascript'],
+        year: 2021
+    },
+    'Gun Game':{
+        url: 'https://github.com/chambres/gungame',
+        description: 'A multiplayer reaction time test website',
+        tags: ['javascript', 'sockets', 'html', 'css'],
+        year: 2021
+    },
+    'Zoom Link UI': {
+        url: 'https://github.com/chambres/ZoomLinkUI',
+        description: 'Generate a UI for your Zoom links',
+        tags: ['javascript', 'html', 'css'],
+        year:2021
+    },
+    'q.reader':{
+        url: 'https://github.com/chambres/q.reader',
+        description: 'Allows you to paste QR codes into browser',
+        tags: ['javascript', 'html', 'css'],
+        year: 2021
+    },
+    'elevation':{
+        url: 'https://github.com/chambres/elevation',
+        description: 'Batch script to elevate prompt in same path',
+        tags: ['batch'],
+        year: 2021
+    },
+    'HerokuHACRedux':{
+        url: 'https://github.com/chambres/HerokuHACRedux',
+        description: "A ground-up remake of KatyISD's Home Access Center in Python",
+        tags: ['python', 'requests', 'flask', 'api'],
+        year: 2021
+    },
+    'unbeatable':{
+        url: 'https://github.com/chambres/unbeatable',
+        description: 'An unbeatable game in Python',
+        tags: ['python'],
+        year: 2018
+    }
+}
+
+
+// window.onload = function() {
+
+// };
+
+function makeCard(title, url, description, tags){
+    return `
+    <div class="card fade-out">
+        <div class="content">
+            <h1 class="title subtitle" style="color: white; font-size: 10px;">${tags.join(', ')}</h1>
+            <h2 class="title">${title}</h2>
+            <p class="copy">${description}</p>
+            <button class="btn" onclick="location.href='${url}'">Open</button>
+        </div>
+    </div>
+    `
+}
+
+function addCardsToDOM(){
+    for(var card in cards){
+        var url = cards[card].url;
+        var description = cards[card].description;
+        var tags = cards[card].tags;
+        console.log(card, url, description, tags)
+        var cardHTML = makeCard(card, url, description, tags);
+        document.getElementsByClassName('page-content')[0].innerHTML += cardHTML;
+    }
+}
+
+function addCardToDOM(card, index){
+    var url = cards[card].url;
+    var description = cards[card].description;
+    var tags = cards[card].tags;
+    var cardHTML = makeCard(card, url, description, tags);
+    document.getElementsByClassName('page-content')[index].innerHTML += cardHTML;
+}
+
+function makeHeader(text){
+    return `
+    <div class="about-border fade-out"></div>
+    <h1 class="fade-out subheader" style="color: aliceblue; font-size: 30px;">${text}</h1>
+    <div class="about-border fade-out"></div>
+    `
+}
+
+function addHeaderToDOM(language){
+    var headerHTML = makeHeader(language);
+    document.body.innerHTML += headerHTML;
+
+}
+
+function addPageContentToDOM(){
+    document.body.innerHTML += `
+    <main class="page-content">
+    
+    </main>
+    `
+}
+
+function deletePageContent(){
+
+     // Get the cards
+     var cards = document.querySelectorAll('.fade-in');
+
+     // Fade out each card one by one
+     cards.forEach((card, index) => {
+         setTimeout(() => {
+             card.classList.add('fade-cards-away');
+             card.addEventListener('animationend', function() {
+                 card.parentNode.removeChild(card);
+             });
+         }, index * 50); // Adjust delay as needed
+     });
+    
+
+    //delete existing page contents
+    var pageContent = document.getElementsByClassName('page-content');
+    while(pageContent.length > 0){
+        pageContent[0].parentNode.removeChild(pageContent[0]);
+    }
+
+    //delete existing 
+    var aboutBorder = document.getElementsByClassName('about-border');
+    while(aboutBorder.length > 0){
+        aboutBorder[0].parentNode.removeChild(aboutBorder[0]);
+    }
+
+    //delete existing
+    var title = document.getElementsByClassName('title');
+    while(title.length > 0){
+        title[0].parentNode.removeChild(title[0]);
+    }
+
+    //delete existing
+    var header = document.getElementsByClassName('subheader');
+    while(header.length > 0){
+        header[0].parentNode.removeChild(header[0]);
+    }
+}
+
+function getActiveButton(){
+
+    if(document.getElementsByClassName('active').length == 0){
+        return 'none';
+    }
+    else{
+        return document.getElementsByClassName('active')[0].id;
+    }
+}
+
+function resetButton(){
+    var buttons = document.getElementsByClassName('fill');
+    for (var i = 0; i < buttons.length; i++) {
+        if (buttons[i] != undefined) {
+            buttons[i].style.backgroundColor = '#000000';
+            buttons[i].style.color = '#ffffff';
+        }
+    }
+    for(var i = 0; i < document.getElementsByClassName('active').length; i++){
+        document.getElementsByClassName('active')[i].classList.remove('active');
+    }
+
+}
+
+function date(){
+    if(getActiveButton() == 'date'){
+        return;
+    }
+
+    resetButton();   
+
+    deletePageContent();
+
+    document.getElementById('date').style.backgroundColor = '#00f3e7';
+    document.getElementById('date').style.color = '#000000';
+
+    document.getElementById('date') .classList.add('active');
+    
+    var sortedByYear = Object.keys(cards).reduce(function(acc, key) {
+        var year = cards[key].year;
+        var name = cards[key]; // Assuming 'name' is the property for the card's name
+        if (!acc[year]) {
+          acc[year] = [];
+        }
+        acc[year].push(key);
+        return acc;
+      }, {});
+      
+    var c = 0;
+
+
+    
+
+    var years = Object.keys(sortedByYear).sort(function(a, b) {
+        return b - a; // Sort in descending order
+      });
+      
+      var c = 0;
+      
+      for (var i = 0; i < years.length; i++) {
+        var year = years[i];
+        addHeaderToDOM(year);
+        addPageContentToDOM();
+        for (var card in sortedByYear[year]) {
+          addCardToDOM(sortedByYear[year][card], c);
+        }
+        c += 1;
+      }
+    
+
+
+    fadeCardsIn();
+    setImagesToCards();
+
+}
+
+function language(){
+    
+
+    if(getActiveButton() == 'language'){
+        return;
+    }
+
+    resetButton();
+    deletePageContent();
+
+    document.getElementById('language').style.backgroundColor = '#00f3e7';
+    document.getElementById('language').style.color = '#000000';
+
+    document.getElementById('language') .classList.add('active');
+
+    const languages = ["python", "javascript", "java", "batch", "unity"];
+    var result = {};
+
+    for(card in cards){
+        for(var lang in languages){
+            if(cards[card].tags.includes(languages[lang])){
+                if(result[languages[lang]] == undefined){
+                    result[languages[lang]] = {
+                        'cards': [card]
+                    }
+                }else{
+                    result[languages[lang]]['cards'].push(card);
+                }
+            }
+        }
+    }
+
+    var c = 0;
+ 
+    for(var language in result){
+        addHeaderToDOM(language.charAt(0).toUpperCase() + language.slice(1)); 
+        addPageContentToDOM(); 
+        for(var card in result[language]['cards']){
+            addCardToDOM(result[language]['cards'][card], c);
+        }
+
+        c+=1;
+        
+    }
+
+    fadeCardsIn();
+    setImagesToCards();
+}
+
+
+
+
+
+function setImagesToCards(){
+    var cards = document.querySelectorAll('.card');
+    c = 0;
+    cards.forEach(function(card, index) {
+        //card.style.backgroundImage = `url(${picture})`;
+        card.style.backgroundColor = `rgb(${50+c}, ${50+c}, ${50+c})`;
+        c+=5;
+    });
+
+}
+
+function fadeCardsIn(){
+    var cards = document.querySelectorAll('.fade-out');
+    cards.forEach(function(card, index) {
+      setTimeout(function() {
+        card.classList.add('fade-in');
+      }, index * 50); // 0.5s delay for each card
+    });
+}
+
+
+//    $('.hamburger').click(function (e) {
+//         $(this).parent().find('ol').toggleClass('active');
+//         $(this).toggleClass('active');
+//         $('.dimmer').toggleClass('active');
+//         $('body').toggleClass('no-scrolling');
+//     });
+
+//     $('li .icon-mobileMenu').click(function () {
+//         $(this).parent().toggleClass('active');
+//     });
+//header stuff
